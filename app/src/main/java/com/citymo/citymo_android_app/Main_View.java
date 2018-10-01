c951a5fa1b3800db46d58f0c8fd9d142d5c28908
jorgemayorga@bitbucket.org <jorgemayorga@bitbucket.org>
2018-09-30 21:18:10 -0500

    Initial App

---
+++ app/src/main/java/com/citymo/citymo_android_app/Main_View.java
@@ -0,0 +1,13 @@
+package com.citymo.citymo_android_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_View extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_view);
+    }
+}
