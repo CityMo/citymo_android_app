46102c5e606bb25b3ecd5cc73acbe50a14224c88
jorgemayorga@bitbucket.org <jorgemayorga@bitbucket.org>
2018-10-21 09:46:22 -0500

    Google Maps Marker in Bogota Patch,
    Running in Simulator in LENOVO JORGE.
    
    This version has a Google Maps API KEY and a new marker in Bogota.
    
    @pm22  please confirm if you can deploy in Android App.

--- app/src/main/java/com/citymo/citymo_android_app/Main_View.java
+++ app/src/main/java/com/citymo/citymo_android_app/Main_View.java
@@ -8,6 +8,6 @@ public class Main_View extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_view);
+        //setContentView(R.layout.activity_main_view);
     }
 }
